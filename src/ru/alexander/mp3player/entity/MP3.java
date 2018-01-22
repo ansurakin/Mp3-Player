f3bc7fe9b65f79042abfe23ac320f5a3d5bec066
Alex <Alex@Alex-PC>
2018-01-22 20:19:51 +0300

    '123'

--- src/ru/alexander/mp3player/entity/MP3.java
+++ src/ru/alexander/mp3player/entity/MP3.java
@@ -3,40 +3,38 @@ package ru.alexander.mp3player.entity;
 import java.io.Serializable;
 import ru.alexander.mp3player.utils.FileUtils;
 
-public class Mp3 implements Serializable{
-    
+public class MP3 implements Serializable{
+
     private String name;
-    
     private String path;
 
-    public Mp3(String name, String path) {
+    public MP3(String name, String path) {
         this.name = name;
         this.path = path;
-    }   
+    }
 
+    
     @Override
+    // для корректного отображения объекта MP3 при добавлении в плейлист
     public String toString() {
-        return FileUtils.getFileNameWithoutExtension(this.name);
+        return FileUtils.getFileNameWithoutExtension(name);
     }
     
-    
-
+   
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public String getPath() {
         return path;
     }
-
+    
     public void setPath(String path) {
         this.path = path;
     }
-    
-    
-
+   
 }
